45ffa7542fa941f8e00298d211b0f409aa48c1a1
Lucas Souza da Fonte <fontelucas@yahoo.com.br>
2019-12-01 19:09:39 -0300

    finished project

--- client/src/pages/Details/index.js
+++ client/src/pages/Details/index.js
@@ -10,7 +10,6 @@ import { Container, Content, Post } from './styles';
 
 export default function Details({ match }) {
     const [posts, setPosts] = useState([]);
-    const [action, setAction] = useState(0);
 
     async function loadPosts() {
         const response = await api.get(`users/${match.params.id}/posts`);
@@ -23,33 +22,9 @@ export default function Details({ match }) {
         }
     }
 
-    async function handleTestPosts() {
-        const testPosts = await axios.get(
-            'http://jsonplaceholder.typicode.com/posts'
-        );
-
-        const response = {
-            posts: testPosts.data
-        };
-
-        Promise.all(
-            response.posts.map(postData => {
-                const thisPost = api.post(
-                    `users/${postData.userId}/posts`,
-                    postData
-                );
-                return thisPost;
-            })
-        );
-
-        if (response) {
-            toast.success('Posts de teste cadastrados');
-        }
-        setAction(action + 1);
-    }
     useEffect(() => {
         loadPosts();
-    }, [action]);
+    }, []);
 
     return (
         <Container>
